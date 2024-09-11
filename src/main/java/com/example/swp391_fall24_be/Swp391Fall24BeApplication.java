6394297db534faf5602a97826fc4e4b8bd20b2d7
nguyendaithinh3004 <nguyendaithinh3004@gmail.com>
2024-09-11 10:18:23 +0700

    first commit

---
+++ src/main/java/com/example/swp391_fall24_be/Swp391Fall24BeApplication.java
@@ -0,0 +1,13 @@
+package com.example.swp391_fall24_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Swp391Fall24BeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Swp391Fall24BeApplication.class, args);
+	}
+
+}
