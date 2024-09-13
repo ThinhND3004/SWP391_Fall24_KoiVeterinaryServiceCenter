e59e830d1f81deeff5344828f6fd3aee32bd9016
nguyendaithinh3004 <nguyendaithinh3004@gmail.com>
2024-09-13 08:39:36 +0700

    update lombok in entity classes

--- src/main/java/com/example/swp391_fall24_be/entities/RoleEntity.java
+++ src/main/java/com/example/swp391_fall24_be/entities/RoleEntity.java
@@ -1,12 +1,14 @@
 package com.example.swp391_fall24_be.entities;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
 @Entity (name = "roles")
+@Data
 public class RoleEntity {
 
     @Id
@@ -27,51 +29,5 @@ public class RoleEntity {
     @OneToMany
     private List<UserEntity> users;
 
-    public UUID getId() {
-        return id;
-    }
 
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public LocalDateTime getCreateAt() {
-        return createAt;
-    }
-
-    public void setCreateAt(LocalDateTime createAt) {
-        this.createAt = createAt;
-    }
-
-    public LocalDateTime getUpdateAt() {
-        return updateAt;
-    }
-
-    public void setUpdateAt(LocalDateTime updateAt) {
-        this.updateAt = updateAt;
-    }
-
-    public List<UserEntity> getUsers() {
-        return users;
-    }
-
-    public void setUsers(List<UserEntity> users) {
-        this.users = users;
-    }
 }
--- src/main/java/com/example/swp391_fall24_be/entities/UserEntity.java
+++ src/main/java/com/example/swp391_fall24_be/entities/UserEntity.java
@@ -1,6 +1,7 @@
 package com.example.swp391_fall24_be.entities;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -8,6 +9,7 @@ import java.util.List;
 import java.util.UUID;
 
 @Entity(name = "users")
+@Data
 public class UserEntity {
 
     @Id
@@ -44,107 +46,5 @@ public class UserEntity {
     @OneToMany
     private List<BookingEntity> booking;
 
-    public List<BookingEntity> getBooking() {
-        return booking;
-    }
 
-    public void setBooking(List<BookingEntity> booking) {
-        this.booking = booking;
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public LocalDate getDob() {
-        return dob;
-    }
-
-    public void setDob(LocalDate dob) {
-        this.dob = dob;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public LocalDateTime getCreateAt() {
-        return createAt;
-    }
-
-    public void setCreateAt(LocalDateTime createAt) {
-        this.createAt = createAt;
-    }
-
-    public LocalDateTime getUpdateAt() {
-        return updateAt;
-    }
-
-    public void setUpdateAt(LocalDateTime updateAt) {
-        this.updateAt = updateAt;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getIsDisable() {
-        return isDisable;
-    }
-
-    public void setIsDisable(String isDisable) {
-        this.isDisable = isDisable;
-    }
-
-    public RoleEntity getRole() {
-        return role;
-    }
-
-    public void setRole(RoleEntity role) {
-        this.role = role;
-    }
 }
