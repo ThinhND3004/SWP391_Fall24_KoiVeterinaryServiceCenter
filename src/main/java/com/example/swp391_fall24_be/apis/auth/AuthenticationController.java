4e1c79ef5ead8033e620820f0a10f4b911f59dfb
Long Pham <longpham5252@gmail.com>
2024-09-14 13:56:43 +0700

    use swagger for auth

--- src/main/java/com/example/swp391_fall24_be/apis/auth/AuthenticationController.java
+++ src/main/java/com/example/swp391_fall24_be/apis/auth/AuthenticationController.java
@@ -2,10 +2,12 @@ package com.example.swp391_fall24_be.apis.auth;
 
 import com.example.swp391_fall24_be.apis.auth.dto.AccountLoginDto;
 import com.example.swp391_fall24_be.apis.auth.dto.AccountLoginResponseDto;
+import com.example.swp391_fall24_be.core.ProjectException;
+import com.example.swp391_fall24_be.core.ResponseDto;
+import io.swagger.v3.oas.annotations.headers.Header;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,26 +22,29 @@ public class AuthenticationController {
     private final AuthenticationService authenticationService;
 
     @PostMapping("login-password")
-    public ResponseEntity<AccountLoginResponseDto> login(@RequestBody AccountLoginDto loginDto) {
+    @ApiResponse(description = "Login with username and password", headers = {
+            @Header(name = "content-type", description = "application/json")
+    })
+    public ResponseDto<AccountLoginResponseDto> login(@RequestBody AccountLoginDto loginDto) {
         try {
             AccountLoginResponseDto response = authenticationService.loginWithUsernameAndPassword(loginDto);
-            HttpHeaders headers = new HttpHeaders();
-            headers.add("content-type", "application/json");
-            return ResponseEntity.ok().headers(headers).body(response);
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+            return new ResponseDto<>(HttpStatus.OK, "Login successfully!", response, null);
+        } catch (ProjectException e) {
+            return new ResponseDto<>(HttpStatus.BAD_REQUEST, "Cannot login!", null, e.getMessage());
         }
     }
 
     @GetMapping("/login-google")
-    public ResponseEntity<AccountLoginResponseDto> loginWithGoogle(@RequestParam String credential) {
+    @ApiResponse(description = "Login with google", headers = {
+            @Header(name = "content-type", description = "application/json")
+    })
+    public ResponseDto<AccountLoginResponseDto> loginWithGoogle(@RequestParam String credential) {
         try {
             AccountLoginResponseDto response = authenticationService.loginWithGoogle(credential);
-            HttpHeaders headers = new HttpHeaders();
-            headers.add("content-type", "application/json");
-            return ResponseEntity.ok().headers(headers).body(response);
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+            return new ResponseDto<>(HttpStatus.OK, "Login with google successfully!", response, null);
+        } catch (ProjectException e) {
+            System.out.println(e.getMessage());
+            return new ResponseDto<>(HttpStatus.BAD_REQUEST, "Cannot login with google!", null, e.getMessage());
         }
     }
 }
