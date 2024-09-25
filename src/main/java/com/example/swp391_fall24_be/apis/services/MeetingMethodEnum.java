673d5b30c8fe6ff5fde94ad28ed85392db554e55
toan1708 <112725866+toan1708@users.noreply.github.com>
2024-09-25 08:42:20 +0700

    Remove Error class

--- src/main/java/com/example/swp391_fall24_be/apis/services/MeetingMethodEnum.java
+++
@@ -1,7 +0,0 @@
-package com.example.swp391_fall24_be.apis.services;
-
-public enum MeetingMethodEnum {
-    ONLINE,
-    OFFLINE_HOME,
-    OFFLINE_CENTER
-}
--- src/main/java/com/example/swp391_fall24_be/apis/services/ServicesRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.swp391_fall24_be.apis.services;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ServicesRepository extends JpaRepository<ServiceEntity, String> {
-}
