9bea6e3a30b9005a859aa79e144d8461efb74ad3
nguyendaithinh3004 <nguyendaithinh3004@gmail.com>
2024-09-25 07:03:42 +0700

    .

--- src/main/java/com/example/swp391_fall24_be/apis/email/EmailService.java
+++ src/main/java/com/example/swp391_fall24_be/apis/email/EmailService.java
@@ -1,12 +1,14 @@
 package com.example.swp391_fall24_be.apis.email;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService {
+
 
     @Autowired
     private JavaMailSender javaMailSender;
