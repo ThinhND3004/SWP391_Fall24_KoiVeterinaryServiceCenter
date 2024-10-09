bb61cc72b0776bef3639d733150f38e0e51f317d
CuonggTV <124784766+CuonggTV@users.noreply.github.com>
2024-10-09 12:05:23 +0700

    setup openapi for security

--- src/main/java/com/example/swp391_fall24_be/config/OpenAPIConfiguration.java
+++ src/main/java/com/example/swp391_fall24_be/config/OpenAPIConfiguration.java
@@ -1,20 +1,41 @@
 package com.example.swp391_fall24_be.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@OpenAPIDefinition
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+@OpenAPIDefinition(
+        info = @Info(
+                description = "OpenApi documentation for Koi Veterian Service Center",
+                title = "Koi Veterian Service Center OpenApi Specification",
+                version = "1.0"
+        ),
+        servers = {
+                @Server(
+                        description = "Local",
+                        url = "http://localhost:8080"
+                ),
+                @Server(
+                        description = "Product",
+                        url = "https://koiveterianservicenter.com"
+                )
+        },
+        security = {
+                @SecurityRequirement(
+                        name = "Authorization"
+                )
+        }
+)
+@SecurityScheme(
+        name = "Authorization",
+        description = "JWT auth",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
 public class OpenAPIConfiguration {
-    @Bean
-    public OpenAPI defineOpenApi() {
-        Info information = new Info()
-                .title("Koi Veterinary Service Center API")
-                .version("1.0")
-                .description("This API exposes endpoints to manage Koi Veterinary Service Center.");
-        return new OpenAPI().info(information);
-    }
 }
--- src/main/java/com/example/swp391_fall24_be/security/SecurityConfig.java
+++ src/main/java/com/example/swp391_fall24_be/security/SecurityConfig.java
@@ -4,14 +4,17 @@ import com.example.swp391_fall24_be.security.filter.CustomerFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
+@Configuration  
 public class SecurityConfig {
     @Autowired
     private CustomerFilter customerFilter;
@@ -38,7 +41,10 @@ public class SecurityConfig {
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // Sets session management policy to stateless
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/**").permitAll() // Permits all requests to any path
-                        .anyRequest().authenticated()) // Requires authentication for any other requests
+                        .requestMatchers(HttpMethod.GET, "/accounts")
+                            .authenticated()
+
+                        .anyRequest().permitAll()) // Requires authentication for any other requests
                 .build(); // Builds the security filter chain
     }
 }
