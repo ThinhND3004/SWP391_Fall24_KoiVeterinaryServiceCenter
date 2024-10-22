73edf49e7a5a5e1a73408d4726a72a81376387cf
Long Pham <longpham5252@gmail.com>
2024-10-22 20:40:20 +0700

    add invitation for veterinarian

--- src/main/java/com/example/swp391_fall24_be/apis/email/EmailController.java
+++ src/main/java/com/example/swp391_fall24_be/apis/email/EmailController.java
@@ -1,6 +1,5 @@
 package com.example.swp391_fall24_be.apis.email;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -27,5 +26,21 @@ public class EmailController {
                                                 @RequestParam String companyWebsite) {
         emailService.sendOrderConfirmationEmail(to, recipientName, serviceName, date, time, location, referenceNumber, amount, paymentMethod, companyName, companyPhone, companyWebsite);
         return new ResponseEntity<>("Order confirmation email sent successfully!", HttpStatus.OK);
+    }
+
+    @PostMapping("/send-invitation-for-veterinarian")
+    public ResponseEntity<?> sendInvitationForVeterinarian(@RequestParam String to,
+                                                        @RequestParam String recipientName,
+                                                        @RequestParam String serviceName,
+                                                        @RequestParam String serviceMethod,
+                                                        @RequestParam String date,
+                                                        @RequestParam String time,
+                                                        @RequestParam String location,
+                                                        @RequestParam String referenceNumber,
+                                                        @RequestParam String companyName,
+                                                        @RequestParam String companyPhone,
+                                                        @RequestParam String companyWebsite) {
+        emailService.sendInvitationForVeterinarian(to, recipientName, serviceName, serviceMethod, date, time, location, referenceNumber, companyName, companyPhone, companyWebsite);
+        return new ResponseEntity<>("Invitation for veterinarian email sent successfully!", HttpStatus.OK);
     }
 }
