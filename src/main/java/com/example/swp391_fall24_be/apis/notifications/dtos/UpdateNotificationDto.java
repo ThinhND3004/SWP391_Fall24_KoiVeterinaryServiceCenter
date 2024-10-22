bf8d6d39319758ddbb308bf1121cded5cb69e16d
Long Pham <longpham5252@gmail.com>
2024-10-22 21:28:43 +0700

    add notification attributes

--- src/main/java/com/example/swp391_fall24_be/apis/notifications/dtos/UpdateNotificationDto.java
+++ src/main/java/com/example/swp391_fall24_be/apis/notifications/dtos/UpdateNotificationDto.java
@@ -2,10 +2,21 @@ package com.example.swp391_fall24_be.apis.notifications.dtos;
 
 import com.example.swp391_fall24_be.apis.notifications.NotificationEntity;
 import com.example.swp391_fall24_be.core.IDto;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDateTime;
 
 public class UpdateNotificationDto implements IDto<NotificationEntity> {
+
+    @NotNull(message = "Read At must not be null!")
+    @JsonProperty("readAt")
+    private LocalDateTime readAt;
+
     @Override
     public NotificationEntity toEntity() {
-        return null;
+        NotificationEntity notification = new NotificationEntity();
+        notification.setReadAt(readAt);
+        return notification;
     }
 }
