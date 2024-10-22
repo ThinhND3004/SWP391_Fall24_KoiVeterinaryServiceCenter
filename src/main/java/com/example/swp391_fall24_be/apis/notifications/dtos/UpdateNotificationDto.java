6109b87ff281f0a6557afbdcbff1eddda50bf06d
Long Pham <longpham5252@gmail.com>
2024-10-22 21:42:51 +0700

    read at can be null

--- src/main/java/com/example/swp391_fall24_be/apis/notifications/dtos/UpdateNotificationDto.java
+++ src/main/java/com/example/swp391_fall24_be/apis/notifications/dtos/UpdateNotificationDto.java
@@ -3,13 +3,11 @@ package com.example.swp391_fall24_be.apis.notifications.dtos;
 import com.example.swp391_fall24_be.apis.notifications.NotificationEntity;
 import com.example.swp391_fall24_be.core.IDto;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDateTime;
 
 public class UpdateNotificationDto implements IDto<NotificationEntity> {
 
-    @NotNull(message = "Read At must not be null!")
     @JsonProperty("readAt")
     private LocalDateTime readAt;
 
