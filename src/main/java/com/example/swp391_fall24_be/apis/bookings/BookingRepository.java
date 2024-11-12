17e0289199e4a04f024554cb9a8904cf20bd4324
CuonggTV <rinjin2004@gmail.com>
2024-11-12 18:13:24 +0700

    add CleanUp Service

--- src/main/java/com/example/swp391_fall24_be/apis/bookings/BookingRepository.java
+++ src/main/java/com/example/swp391_fall24_be/apis/bookings/BookingRepository.java
@@ -1,7 +1,11 @@
 package com.example.swp391_fall24_be.apis.bookings;
 
 import com.example.swp391_fall24_be.apis.accounts.AccountEntity;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -12,4 +16,9 @@ public interface BookingRepository extends JpaRepository<BookingEntity, String> 
     List<BookingEntity> findByVeterianAndStatusEnum(AccountEntity veterian, StatusEnum statusEnum2);
 
     List<BookingEntity> findAllByStartedAtBetween(LocalDateTime startedAt, LocalDateTime endedAt);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE bookings b SET b.statusEnum = 'CANCELED' WHERE b.startedAt < :expiryDate AND b.statusEnum = 'PENDING'")
+    void updateOutdatedBooking(@Param("expiryDate") LocalDateTime expiryDate);
 }
