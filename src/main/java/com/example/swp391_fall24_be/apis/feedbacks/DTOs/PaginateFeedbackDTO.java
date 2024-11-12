b0325b55128ca40900ecbbcf60c477d69463b51b
CuonggTV <124784766+CuonggTV@users.noreply.github.com>
2024-11-12 08:43:13 +0700

    add paginate for feedbacks

--- src/main/java/com/example/swp391_fall24_be/apis/feedbacks/DTOs/PaginateFeedbackDTO.java
+++ src/main/java/com/example/swp391_fall24_be/apis/feedbacks/DTOs/PaginateFeedbackDTO.java
@@ -1,15 +1,30 @@
 package com.example.swp391_fall24_be.apis.feedbacks.DTOs;
 
+import com.example.swp391_fall24_be.apis.accounts.AccountEntity;
+import com.example.swp391_fall24_be.apis.accounts.AccountRoleEnum;
+import com.example.swp391_fall24_be.apis.bookings.BookingEntity;
 import com.example.swp391_fall24_be.apis.feedbacks.Feedback;
 import com.example.swp391_fall24_be.core.AbstractPagination;
 
+import java.beans.ConstructorProperties;
+
 public class PaginateFeedbackDTO extends AbstractPagination<Feedback> {
-    public PaginateFeedbackDTO(Integer page, Integer unitPerPage) {
+    public String bookingId;
+
+    @ConstructorProperties({"page", "unitPerPage", "bookingId"})
+    public PaginateFeedbackDTO(Integer page, Integer unitPerPage, String bookingId) {
         super(page, unitPerPage);
+        this.bookingId = bookingId;
     }
 
     @Override
     public Feedback toEntity() {
-        return null;
+        Feedback feedback = new Feedback();
+
+        BookingEntity booking = new BookingEntity();
+        booking.setId(bookingId);
+        feedback.setBooking(booking);
+
+        return feedback;
     }
 }
